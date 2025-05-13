547a0717cb936934ab815186c916ae3d1737b731
dungpd2003 <dung.pd215265@sis.hust.edu.vn>
2025-05-13 10:35:33 +0700

    update itinerary and tour details

--- DATN/FE/src/AppProvider.jsx
+++ DATN/FE/src/AppProvider.jsx
@@ -1,8 +1,10 @@
 import { AuthProvider } from '@/contexts/AuthContext';
+import { BookingProvider } from '@/contexts/BookingContext';
+import { ItineraryProvider } from '@/contexts/ItineraryContext';
 import { TourProvider } from '@/contexts/TourContext';
 import { UserProvider } from '@/contexts/UserContext';
 
-const providers = [AuthProvider, UserProvider, TourProvider];
+const providers = [AuthProvider, UserProvider, TourProvider, ItineraryProvider, BookingProvider];
 
 export function AppProvider({ children }) {
   return providers.reduceRight((acc, Provider) => {
